28cb9c6264c48b2d715a1c83717e639528c02331
maicazapanta <maicazapanta@github.com>
2019-09-03 18:31:59 +0800

    Updates

--- precache-manifest.af782a7b5e1eac9fedc133995cd85eb1.js
+++ precache-manifest.af782a7b5e1eac9fedc133995cd85eb1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6665095281b3d62f50ac786c20b4c0b6",
+    "revision": "e3e40c7658f6484cbdbd7d62c040479f",
     "url": "/projects/index.html"
   },
   {
@@ -8,15 +8,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/projects/static/css/2.848db5fd.chunk.css"
   },
   {
-    "revision": "065358ee23c582f6697e",
-    "url": "/projects/static/css/main.a343c59e.chunk.css"
+    "revision": "33ca1d3f5a2f3b29710e",
+    "url": "/projects/static/css/main.18b57409.chunk.css"
   },
   {
     "revision": "ada61cef529d829e075e",
     "url": "/projects/static/js/2.76afc4f8.chunk.js"
   },
   {
-    "revision": "065358ee23c582f6697e",
+    "revision": "33ca1d3f5a2f3b29710e",
     "url": "/projects/static/js/main.2e574ad1.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/projects/precache-manifest.6ff1a632085ab7984ceb71d7b85569fb.js"
+  "/projects/precache-manifest.af782a7b5e1eac9fedc133995cd85eb1.js"
 );
 
 self.addEventListener('message', (event) => {
